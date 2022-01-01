ba242c50931b9dea3bbc4c4e9651b47992a07b6d
Mihai-Cristian Bâltac <baltacmihaicristian@gmail.com>
2022-01-01 16:48:35 +0200

    filter works

--- frontend/food-waste/src/components/FoodMenu.jsx
+++ frontend/food-waste/src/components/FoodMenu.jsx
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+
+import CardItems from "./CardItems";
 
 function FoodMenu(props) {
-  return returnFoodMenu(props.userId, props.isYou, props.userName);
+  return ReturnFoodMenu(props.userId, props.isYou, props.userName);
 }
 
-function returnFoodMenu(id, isYou, name) {
+function ReturnFoodMenu(id, isYou, name) {
+  const [refresh, setRefresh] = useState(false);
   return (
     <div className="foodMenu">
       <p className="foodMenu__title">{titleDecider(isYou, name)}</p>
-      {returnFilters(isYou)}
+      {returnFilters(isYou, refresh, setRefresh)}
+      {ReturnItems(id, refresh, setRefresh)}
     </div>
   );
 }
@@ -22,19 +26,25 @@ function titleDecider(isYou, name) {
   }
 }
 
-function returnFilters(isYou) {
+function returnFilters(isYou, value, action) {
   if (isYou)
     return (
       <div className="foodMenu__filters">
-        {returnCategories("All Categories")}
+        <div className="foodMenu__filters__categories">
+          {returnCategories("All Categories", value, action)}
+        </div>
 
-        <div className="foodMenu__filters__menu"></div>
+        <div className="foodMenu__filters__sort">
+          <div className="foodMenu__filters__add">
+            <span className="icon-plus"></span>
+          </div>
+        </div>
       </div>
     );
 }
 
 //#region functions for the  Categories
-function returnCategories(current) {
+function returnCategories(current, value, action) {
   const categories = generateCategories(current);
   let isFirst = true;
 
@@ -43,9 +53,10 @@ function returnCategories(current) {
       isFirst = !isFirst;
       return (
         <p
-          className="card__editItem__content__categories__category card__editItem__content__categories__category--current"
+          className="foodMenu__filters__categories__category foodMenu__filters__categories__category--current"
           onClick={(e) => {
             selectCategory(e);
+            action(!value);
           }}
         >
           {element}
@@ -54,9 +65,10 @@ function returnCategories(current) {
     } else
       return (
         <p
-          className="card__editItem__content__categories__category "
+          className="foodMenu__filters__categories__category "
           onClick={(e) => {
             selectCategory(e);
+            action(!value);
           }}
         >
           {element}
@@ -68,33 +80,18 @@ function returnCategories(current) {
 function selectCategory(e) {
   const event = e.target;
 
-  const newCategory = document.getElementById("newCategory");
-
-  newCategory.style.display = "none";
-
-  const addNewCategory = document.getElementById("addNewCategory");
-
-  addNewCategory.style.display = "flex";
-
   clearSelectedCategory();
 
-  event.classList.add("card__editItem__content__categories__category--current");
+  event.classList.add("foodMenu__filters__categories__category--current");
 }
 
 function clearSelectedCategory() {
   const removeClass = document.getElementsByClassName(
-    "card__editItem__content__categories__category--current"
-  );
-
-  const addNewCategory = document.getElementById("addNewCategory");
-  addNewCategory.classList.remove(
-    "card__editItem__content__categories__category--current"
+    "foodMenu__filters__categories__category--current"
   );
 
   for (let remove of removeClass) {
-    remove.classList.remove(
-      "card__editItem__content__categories__category--current"
-    );
+    remove.classList.remove("foodMenu__filters__categories__category--current");
   }
 }
 
@@ -103,24 +100,75 @@ function generateCategories(current) {
 
   categories.unshift(current);
 
-  return remove_duplicates(categories);
+  return categories;
 }
 
-function remove_duplicates(arr) {
-  let obj = {};
-  let ret_arr = [];
-  if (arr) {
-    for (let i = 0; i < arr.length; i++) {
-      obj[arr[i]] = true;
-    }
-    for (let key in obj) {
-      ret_arr.push(key);
-    }
+//#endregion
 
-    return ret_arr;
+//#region getItems
+
+function ReturnItems(id, refresh, setRefresh) {
+  let items = CreateItemsList(id, refresh, setRefresh);
+
+  if (items)
+    return (
+      <div className="foodMenu__items">
+        {returnItemList(items, refresh, setRefresh)}
+      </div>
+    );
+}
+
+function returnItemList(data, bool, refresh) {
+  return data
+    ?.filter(decideFilter)
+    .sort(compareDates)
+
+    .map((item) => {
+      return <CardItems item={item} refresh={refresh} val={bool} />;
+    });
+}
+
+function compareDates(first, second) {
+  const firstDate = new Date(first.expirationDate);
+  const secondDate = new Date(second.expirationDate);
+
+  return firstDate > secondDate ? 1 : -1;
+}
+
+function decideFilter(element) {
+  const category = document.getElementsByClassName(
+    "foodMenu__filters__categories__category--current"
+  )[0].innerHTML;
+
+  if (category == "All Categories") {
+    return true;
+  } else return element.category == category ? true : false;
+}
+
+// we get through API the items based on current user
+function CreateItemsList(id, refresh, setRefresh) {
+  let temp = refresh;
+  let [item, setItem] = useState(null);
+
+  const URL = "http://localhost:8081/api/item/getAllItemsByUserId/" + id;
+  if (item == null || temp == true)
+    fetch(URL, {
+      method: "GET",
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        setRefresh(false);
+        temp = false;
+        setItem(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+  if (item != null) {
+    return item;
   }
 }
-
 //#endregion
 
 export default FoodMenu;
